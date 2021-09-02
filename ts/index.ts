88cf9ef6c16cf253a5ba9264205126fa7aad6851
hiro <hiromhiro617@gmail.com>
2021-09-02 11:36:12 +0900

    UI

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,1 @@
-import { User } from "./User";
 
-const user = new User('海老原', '賢次', 44);
-
-const contentsElem = document.getElementById('contents');
-if(!!contentsElem) {
-    contentsElem.innerText = `${user.familyName} ${user.givenName}`;
-}
